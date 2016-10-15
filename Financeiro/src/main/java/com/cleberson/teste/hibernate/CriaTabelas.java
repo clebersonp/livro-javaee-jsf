7c12875dca23b86af61673ceeef9d4d96c7dbced
cleberson <pauluci.cleberson@gmail.com>
2016-10-15 14:33:34 -0300

    criada classe para testar conexao com o banco via hibernate

---
+++ Financeiro/src/main/java/com/cleberson/teste/hibernate/CriaTabelas.java
@@ -0,0 +1,11 @@
+package com.cleberson.teste.hibernate;
+
+import javax.persistence.Persistence;
+
+public class CriaTabelas {
+
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("FinanceiroPU");
+	}
+	
+}
