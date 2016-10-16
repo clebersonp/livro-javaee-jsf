19f92fd17cf3f9030d53beb4da8c384249338a35
cleberson <pauluci.cleberson@gmail.com>
2016-10-16 19:09:56 -0200

    adicionada anotacao no managedBean

--- Financeiro/src/main/java/com/cleberson/financeiro/bean/ConsultaLancamentoBean.java
+++ Financeiro/src/main/java/com/cleberson/financeiro/bean/ConsultaLancamentoBean.java
@@ -3,12 +3,16 @@ package com.cleberson.financeiro.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import com.cleberson.financeiro.model.Lancamento;
 import com.cleberson.financeiro.util.JpaUtil;
 
+@ManagedBean
+@ViewScoped
 public class ConsultaLancamentoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
