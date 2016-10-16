632822d4dca14f87334db37c80697158542ac083
cleberson <pauluci.cleberson@gmail.com>
2016-10-16 18:34:31 -0200

    criado bean para fazer a consulta de lancamentos

---
+++ Financeiro/src/main/java/com/cleberson/financeiro/bean/ConsultaLancamentoBean.java
@@ -0,0 +1,28 @@
+package com.cleberson.financeiro.bean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import com.cleberson.financeiro.model.Lancamento;
+import com.cleberson.financeiro.util.JpaUtil;
+
+public class ConsultaLancamentoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<Lancamento> lancamentos;
+	
+	public void consultar() {
+		EntityManager manager = JpaUtil.getEntityManager();
+		
+		TypedQuery<Lancamento> query = manager.createQuery("from Lancamento", Lancamento.class);
+		this.lancamentos = query.getResultList();
+	}
+
+	public List<Lancamento> getLancamentos() {
+		return lancamentos;
+	}
+}
